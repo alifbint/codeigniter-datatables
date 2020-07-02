fe8d8938eac431066ad1814c8ddfe9d1d824a579
Alif Bintoro <Alif Bintoro>
2020-07-02 15:00:41 +0700

    Add Group By Function

--- application/libraries/Datatables.php
+++ application/libraries/Datatables.php
@@ -1,23 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+* =============================================
+* Datatables for Codeigniter 3.x
+* Version           : 1.0
+* Modified By       : Alif Bintoro <alifbintoro77@gmail.com>
+*
+* Original Source   : Ignited Datatables
+* URL               : https://github.com/IgnitedDatatables/Ignited-Datatables
+* Created By        : Philip Sturgeon <email@philsturgeon.co.uk>
+* =============================================
+*/
 
 class Datatables {
     protected $CI;
-    private $csrfEnable = FALSE;
-    private $table;
-    private $column;
-    private $column_search = array();
-    private $order = array();
-    private $joins = array();
-    private $where = array();
-    private $queryCount;
-    private $type = null;
+    protected $csrfEnable = FALSE;
+    protected $table;
+    protected $column;
+    protected $column_search = array();
+    protected $order = array();
+    protected $joins = array();
+    protected $where = array();
+    protected $group_by = null;
+    protected $queryCount;
 
-    public function __construct($config = array())
+    public function __construct()
     {
         $this->CI =& get_instance();
         $this->csrfEnable = $this->CI->config->item('csrf_protection');
-        if(!empty($config['ui_type']))
-            $this->type = $config['ui_type'];
     }
 
     protected function balanceChars($str, $open, $close)
@@ -60,25 +69,35 @@ class Datatables {
                 $this->column_search[] = $search;
             }
         }
+
+        return $this;
     }
 
     public function from($table){
         $this->table = $table;
+
+        return $this;
     }
 
     public function join($table, $fk, $type = NULL)
     {
         $this->joins[] = array($table, $fk, $type);
+
+        return $this;
     }
 
     public function where($key_condition, $val = NULL)
     {
         $this->where[] = array($key_condition, $val, 'and');
+
+        return $this;
     }
 
     public function or_where($key_condition, $val = NULL)
     {
         $this->where[] = array($key_condition, $val, 'or');
+
+        return $this;
     }
 
     public function order_by($column, $order = 'ASC')
@@ -89,6 +108,15 @@ class Datatables {
         else{
             $this->order[$column] = $order;
         }
+
+        return $this;
+    }
+
+    public function group_by($group_by)
+    {
+        $this->group_by = $group_by;
+
+        return $this;
     }
 
     protected function _get_datatables_query()
@@ -112,6 +140,9 @@ class Datatables {
                 }
             }
         }
+
+        if(!empty($this->group_by))
+            $this->CI->db->group_by($this->group_by);
 
         $tmpQueryCount = str_replace($this->column, 'COUNT(1)', str_replace('`', '', $this->CI->db->get_compiled_select($this->table, false)));
 
@@ -141,7 +172,7 @@ class Datatables {
          
         if(isset($_POST['order'])){
             $this->CI->db->order_by($this->column_search[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-        }
+        } 
         else if(!empty($this->order)){
             foreach($this->order as $key => $val)
                 $this->CI->db->order_by($key, $val);
@@ -157,12 +188,12 @@ class Datatables {
         return $this->CI->db->get()->result_array();
     }
  
-    protected function count_total()
+    public function count_total()
     {
         return $this->CI->db->query($this->queryCount)->row_array();
     }
 
-    public function generate()
+    public function generate($raw = false)
     {
         $list = $this->get_datatables();
         $total = $this->count_total();
@@ -185,229 +216,12 @@ class Datatables {
         if($this->csrfEnable)
             $output['csrf_token'] = $this->CI->security->get_csrf_hash();
 
-        header('Content-Type: application/json');
-        echo json_encode($output);
-    }
-
-    public function css($type = null)
-    {
-        if(!empty($this->type)){
-            $type = $this->type
+        if($raw){
+            return $output;
         }
         else{
-            $this->type = $type;
+            header('Content-Type: application/json');
+            echo json_encode($output);
         }
-
-        switch($type){
-            case 'bootstrap':
-            case 'bootstrap3':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap.min.css">';
-            break;
-
-            case 'bootstrap4':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap4.min.css">';
-            break;
-
-            case 'foundation':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.foundation.min.css">';
-            break;
-
-            case 'jquery':
-            case 'jqueryui':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.jqueryui.min.css">';
-            break;
-
-            case 'material':
-            case 'materialui':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.material.min.css">';
-            break;
-
-            case 'semantic':
-            case 'semanticui':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.semanticui.min.css">';
-            break;
-
-            case 'uikit':
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.uikit.min.css">';
-            break;
-
-            default:
-                echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/jquery.dataTables.min.css">';
-            break;
-        }
-    }
-
-    public function js($type = null)
-    {
-        if(!empty($this->type)){
-            $type = $this->type
-        }
-        else{
-            $this->type = $type;
-        }
-
-        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/jquery.dataTables.min.js"></script>';
-        switch($type){
-            case 'bootstrap':
-            case 'bootstrap3':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.bootstrap.min.js"></script>'
-            break;
-
-            case 'bootstrap4':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.bootstrap4.min.js"></script>'
-            break;
-
-            case 'foundation':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.foundation.min.js"></script>'
-            break;
-
-            case 'jquery':
-            case 'jqueryui':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.jqueryui.min.js"></script>'
-            break;
-
-            case 'material':
-            case 'materialui':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.material.min.js"></script>'
-            break;
-
-            case 'semantic':
-            case 'semanticui':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.semanticui.min.js"></script>'
-            break;
-
-            case 'uikit':
-                '<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.uikit.min.js"></script>'
-            break;
-        }
-    }
-
-    public function generate_js($idDom, $url, $config = array())
-    {
-        echo 'var '.str_replace(array('-','_'), array('',''), $idDom);
-        if($this->csrfEnable)
-            echo 'var csrfData = "'.$this->CI->security->get_csrf_hash().'";';
-        echo '$.fn.dataTable.ext.errMode = "none";';
-        echo '$(document).ready(function(){';
-        echo str_replace(array('-','_'), array('',''), $idDom).' = $("#'.$idDom.'").DataTable({';
-        echo 'processing: true,';
-        echo 'serverSide: true,';
-        echo 'ajax: {';
-        echo 'url: "'.$url.'",';
-        echo 'type: "POST",';
-        if($this->csrfEnable){
-            echo 'data: function(d){';
-            echo 'return $.extend( {}, d, {';
-            echo '"'.$this->CI->security->get_csrf_token_name().'": csrfData';
-            echo '});';
-            echo '},';
-            echo 'dataSrc:function(response) {';
-            echo 'csrfData = response.csrf_token;';
-            echo 'return response.data;';
-            echo '},';
-            echo '},';
-        }
-        echo 'ordering: '.(($config['ordering'])?true:false).',';
-        echo 'searching: '.(($config['searching'])?true:false).',';
-        echo 'pageLength: '.((!empty($config['pageLength']))?$config['pageLength']:25).',';
-        echo 'responsive: '.(($config['responsive'])?true:false).',';
-        echo ((!empty($config['language']))?'language: '.json_encode($config['language']).',':null);
-        if(!empty($config['order'])){
-            $tmpI = 1;
-            echo 'order: [';
-            foreach($config['order'] as $key => $value){
-                if(count($config['order']) == $tmpI){
-                    echo '['.$key.', "'.$value.'"]';
-                }
-                else{
-                    echo '['.$key.', "'.$value.'"],';
-                }
-                $tmpI++;
-            }
-            echo '],';
-        }
-        else{
-            echo 'order: [],';
-        }
-        echo 'columns: [';
-        $tmpI = 1;
-        foreach($config['columns'] as $result){
-            echo '{ ';
-            $tmpJ = 1;
-
-            foreach($result as $key => $val){
-                if(count($result) == $tmpJ){
-                    if($key == 'mRender'){
-                        echo $key.': '.$val;
-                    }
-                    else{
-                        echo $key.': '.((gettype($val) == 'string')?'"'.$val.'"':$val);
-                    }
-                }
-                else{
-                    if($key == 'mRender'){
-                        echo $key.': '.$val.',';
-                    }
-                    else{
-                        echo $key.': '.((gettype($val) == 'string')?'"'.$val.'"':$val).',';
-                    }
-                }
-                $tmpJ++;
-            }
-
-            if(count($config['columns']) == $tmpI){
-                echo ' }';
-            }
-            else{
-                echo ' },';
-            }
-            $tmpI++;
-        }
-        if(!empty($config['columnDefs'])){
-            echo '],';
-            echo 'columnDefs:[';
-            $tmpI = 1;
-            foreach($config['columnDefs'] as $result){
-                echo '{';
-                $tmpJ = 1;
-                foreach($result as $key => $val){
-                    if(count($result) == $tmpJ){
-                        if($key == 'targets'){
-                            echo $key.': ['.$val.']';
-                        }
-                        elseif($key == 'render'){
-                            echo $key.': '.$val;
-                        }
-                        else{
-                            echo $key.': '.((gettype($val) == 'string')?'"'.$val.'"':$val);
-                        }
-                    }
-                    else{
-                        if($key == 'targets'){
-                            echo $key.': ['.$val.'],';
-                        }
-                        elseif($key == 'render'){
-                            echo $key.': '.$val.',';
-                        }
-                        else{
-                            echo $key.': '.((gettype($val) == 'string')?'"'.$val.'"':$val).',';
-                        }
-                    }
-                    $tmpJ++;
-                }
-                if(count($config['columnDefs']) == $tmpI){
-                    echo '}';
-                }
-                else{
-                    echo '},';
-                }
-            }
-            echo ']';
-        }
-        else{
-            echo ']';
-        }
-        echo '});';
-        echo '});';
     }
 }
